12b96d606c242f94ad7bf35b81341bf2bc0786df
Koki Watanabe <56009584+math-hiyoko@users.noreply.github.com>
2026-01-27 22:27:41 +0900

    Change BlockType (#23)
    * change BlockType
    
    * format
    
    * update version

--- src/utils/bit_vector.rs
+++ src/utils/bit_vector.rs
@@ -10,8 +10,8 @@ use serde::{Deserialize, Serialize};
 
 use crate::utils::bit_select::BitSelect;
 
-pub(super) type BlockType = u64;
-const SELECT_INDEX_INTERBVAL: usize = 1 << 13;
+pub(super) type BlockType = u128;
+const SELECT_INDEX_INTERBVAL: usize = 1 << 14;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub(super) struct BitVector {
@@ -88,8 +88,9 @@ impl BitVector {
         let (block_index, bit_index) = end.div_rem(&(BlockType::BITS as usize));
         let mut rank = self.ranks[block_index];
         if block_index < self.blocks.len() {
-            rank += (self.blocks[block_index] & ((1 << bit_index) - BlockType::one())).count_ones()
-                as usize;
+            rank += (self.blocks[block_index]
+                & ((BlockType::one() << bit_index) - BlockType::one()))
+            .count_ones() as usize;
         }
         Ok(rank)
     }
